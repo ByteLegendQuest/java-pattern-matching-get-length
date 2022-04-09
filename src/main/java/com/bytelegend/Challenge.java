74ccc1251608af472eb19dfe023209d6ffc68aa1
liubaolai <1164552059@qq.com>
2022-04-09 15:29:51 +0000

    Update at 2022-04-09T15:29:51.284896Z

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -28,6 +28,12 @@ public class Challenge {
      * Matching for switch](https://openjdk.java.net/jeps/406)**
      */
     public static Integer getLength(Object obj) {
-        return 0;
+        return switch(obj) {
+            case null ->  null;
+            case Collection c ->  c.size();
+            case Map m ->  m.size();
+            case Object[] ia ->  ia.length;
+            default -> -1
+        }
     }
 }
